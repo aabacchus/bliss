bc4684a09955cbbccbb1a109ddd57a850e72c901
phoebos <ben@bvnf.space>
2023-06-27 17:18:57 +0100

    fix trailing whitespace

--- bliss/archive.lua
+++ bliss/archive.lua
@@ -7,7 +7,7 @@ local function tar_extract(tarball)
     if not utils.run("tar xf '" .. tarball .. "'") then
         utils.die("failed to extract "..tarball)
     end
-    
+
     local top = dirent.dir()
     if #top > 3 then utils.die("more than 1 top-level directory in tarball " .. tarball) end
     for _,v in ipairs(top) do if v ~= '.' and v ~= '..' then top = v break end end
@@ -16,7 +16,7 @@ local function tar_extract(tarball)
     for _,file in ipairs(d) do
         if file ~= '.' and file ~= '..' then
             assert(file:sub(1,1) ~= '/')
-            local ok, e = stdio.rename(top..'/'..file, file) 
+            local ok, e = stdio.rename(top..'/'..file, file)
             if not ok then
                 utils.die("couldn't rename " .. file .. ": " .. e)
             end
