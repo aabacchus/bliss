538bd0b2145e1718ae46ccabf8cb5522d403a9a2
phoebos <ben@bvnf.space>
2024-12-18 11:41:13 +0000

    list: check env.sys_db exists before dirent.files

--- bliss/list.lua
+++ bliss/list.lua
@@ -2,12 +2,13 @@
 -- @module bliss.list
 local pkg = require "bliss.pkg"
 local dirent = require "posix.dirent"
+local sys_stat = require "posix.sys.stat"
 
 --- The list action.
 -- @tparam env env
 -- @tparam table arg list of packages to search. If none, list all packages.
 local function list(env, arg)
-    if #arg == 0 then
+    if #arg == 0 and sys_stat.stat(env.sys_db) then
         for file in dirent.files(env.sys_db) do
             if string.sub(file, 1, 1) ~= "." then
                 table.insert(arg, file)
